069cfdd81737064f03b122e3495def47aa7b7dd5
Ashique Ali <60208343+ashiqaliaslam@users.noreply.github.com>
2023-05-14 23:49:48 +0500

    feat: hello world app with riverpod

---
+++ hello_world_riverpod/android/app/src/main/kotlin/com/example/hello_world_riverpod/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hello_world_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hello_world_riverpod/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// We create a "provider", which will a value (here "Hello wordl").
+// By using a provder, this allows us to mock/override the value exposed.
+final helloWorldProvider = Provider((_) => 'Hello World!');
+void main() {
+  // For widgets to be able to read providers, we need to wrap the entire
+  // application in a "ProviderScope" sidget.
+  // This is where state of out providers will be store
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+// Extend ConsumerWidget instead of StatelesWidget, which is exposed by Riverpod
+class MyApp extends ConsumerWidget {
+  //  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final String value = ref.watch(helloWorldProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Example')),
+        body: Center(
+          child: Text(
+            value,
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+        ),
+      ),
+    );
+  }
+}
